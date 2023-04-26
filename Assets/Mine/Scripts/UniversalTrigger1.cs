ac5054012b33da8bec46b4f77a316184742f9c2c
Mathiasjb1 <mathiasjb@hotmail.com>
2023-04-26 14:27:51 +0200

    FineTune

--- Assets/Mine/Scripts/UniversalTrigger1.cs
+++ Assets/Mine/Scripts/UniversalTrigger1.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class UniversalTrigger1 : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject activateThis;
+    private void OnTriggerEnter(Collider other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        activateThis.SetActive(true);
     }
 }
